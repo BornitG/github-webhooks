012ccbabfb8d79947a7e262ae8524f0d08043021
Bornit <alghenny@hotmail.com>
2024-06-04 18:33:33 -0400

    Added validation for x-hub-signature-256 from github

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -5,5 +5,7 @@ export const envs = {
 
     PORT: get('PORT').required().asPortNumber(),
 
-    DISCORD_WEBHOOK_URL: get('DISCORD_WEBHOOK_URL').required().asUrlString()
+    DISCORD_WEBHOOK_URL: get('DISCORD_WEBHOOK_URL').required().asUrlString(),
+
+    SECRET_TOKEN: get('SECRET_TOKEN').required().asString()
 }
